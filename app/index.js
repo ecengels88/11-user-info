e2a8f629133578e3efdda1ccdbe533ad569d07d5
Evan Engels <evan.engels@gmail.com>
2017-02-15 20:19:59 -0600

    added JS

--- app/index.js
+++ app/index.js
@@ -1,2 +1,18 @@
 import 'whatwg-fetch';
 import data from './data';
+
+
+const photoID = document.querySelector('.user-image__image');
+photoID.src = data.results[0].picture.large;
+
+const userName = document.querySelector('.name');
+userName.innerText = data.results[0].name.first;
+
+const userMail = document.querySelector('.email');
+userMail.innerText = data.results[0].email;
+
+const userPhone = document.querySelector('.phone');
+userPhone.innerText = data.results[0].cell;
+
+const userLocation = document.querySelector('.location');
+userLocation.innerText = data.results[0].location.city;
